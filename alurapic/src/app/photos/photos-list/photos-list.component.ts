2d51c4fe58b4e0845c6caa29443e002ac4b209b4
Henrique da Silva Lima <51914997+henrikesilva@users.noreply.github.com>
2020-02-07 10:02:29 -0300

    New folders in projects

---
+++ alurapic/src/app/photos/photos-list/photos-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../photo/photo.service';
+import { ActivatedRoute } from '@angular/router';
+import { Photo } from '../photo/photo';
+
+@Component({
+  selector: 'app-photos-list',
+  templateUrl: './photos-list.component.html',
+  styleUrls: ['./photos-list.component.css']
+})
+export class PhotosListComponent implements OnInit {
+
+  photos: Photo[] = [];
+  
+  constructor(
+    private photoService : PhotoService,
+    private activatedRoute : ActivatedRoute 
+  ){}
+  
+  ngOnInit() : void{
+    const userName = this.activatedRoute.snapshot.params.userName;
+
+    this.photoService
+      .listFromUser(userName)
+      .subscribe(photos => this.photos = photos);
+  }
+
+}
